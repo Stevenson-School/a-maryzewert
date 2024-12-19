f1d40cfe053d8ef07c11488900f4fc166d2d1475
maryzewert <maryzewert@gmail.com>
2024-12-18 21:55:21 -0800

    Update MaxArrayDeque.java

--- MaxArrayDeque.java
+++ MaxArrayDeque.java
@@ -1,45 +1,82 @@
 import java.util.Comparator;
 
-public class MaxArrayDeque<T> implements Deque<T>,Comparable<T>{
-    private T[] arr =  (T[]) new Object[8];
-    private int size;
-    protected int first;
-    protected int last;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+    private Comparator<T> comp;
 
     public MaxArrayDeque(Comparator<T> c){
-        arr = new
-      size = 0;
+        comp = c;
     }
 
-    public void addFirst(T item){
+    public MaxArrayDeque(){}
 
-    }
-    public void addLast(T item){
-
-    }
-    public boolean isEmpty(){
-      return size ==0;
-    }
-    public int size(){
-      return size;
-    }
-    public T removeFirst(){
-
-    }
-    public T removeLast(){
-
-    }
-    public T get(int index){
-
-    }
-    public void printDeque(){
-
-    }
     public T max(){
-
+        if(isEmpty()){
+            return null;
+        }
+        T max = get(first+1);
+       for(int i =0;i<size();i++){
+            if(comp.compare(get(i),max)>0){
+                max = get(i);
+            }
+        }
+        return max;
     }
 
     public T max(Comparator<T> c){
+        if(isEmpty()){
+            return null;
+        }
 
+       T max = get(first+1);
+        for(T item:this){
+           if(c.compare(item,max)>0){
+               max = item;
+            }
+        }
+        for(int i =1;i<size();i++){
+            if(c.compare(get(i),max)>0){
+                max = get(i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        Comparator<Double> dc = new Comparator<Double>(){
+            @Override
+            public int compare(Double o1,Double o2){
+                return ((int) (o1-o2));
+            }
+        };
+
+
+        MaxArrayDeque<Double> deck = new MaxArrayDeque<>(dc);
+        deck.addLast(2.3);
+        deck.addLast(0.0);
+        deck.addFirst(4.7);
+        deck.addLast(2.3);
+        deck.addLast(0.0);
+        deck.addFirst(4.7);
+        deck.addLast(2.3);
+        deck.addLast(0.0);
+        deck.resize(2);
+        deck.addFirst(4.7);
+      // System.out.println(deck);
+       System.out.println(deck.toString());
+
+        MaxArrayDeque<Double> deck2 = new MaxArrayDeque<>(dc);
+        deck2.addLast(2.3);
+        deck2.addLast(0.0);
+        deck2.addFirst(4.7);
+        deck2.addLast(2.3);
+        deck2.addLast(0.0);
+        deck2.addLast(5.0);
+      //deck2.addFirst(8.0);
+       System.out.println(deck2.max());
+        System.out.println(deck2);
+        System.out.println(deck2.get(2));
     }
 }
+
